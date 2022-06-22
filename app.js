dc8cd7deea4f65d451b0729454e6b3b62f149806
Tran Truong Van <Kingchenobama711@gmail.com>
2022-06-22 19:41:02 +0700

    level 2 , Encyption

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //jshint esversion:6
+require("dotenv").config();
 const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 
 
 
@@ -28,6 +30,9 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+
 const User = mongoose.model('User', userSchema);
 
 
@@ -91,3 +96,8 @@ app.post("/register", (req, res) => {
 app.listen(3000, () => {
   console.log("listening on port 3000");
 })
+
+
+
+
+
