3ddaefb5347794a6a354cf8e506d63edab9f5867
Tran Truong Van <Kingchenobama711@gmail.com>
2022-06-23 09:15:34 +0700

    Complete Feature 1.0

--- app.js
+++ app.js
@@ -26,9 +26,19 @@ mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
 
 
+const secretSchema = new mongoose.Schema({
+  content: String,
+})
+
+const Secret = mongoose.model("Secret", secretSchema);
+
+
+
+
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  secrets: [secretSchema]
 });
 
 const User = mongoose.model('User', userSchema);
@@ -56,13 +66,21 @@ app.get("/register", (req, res) => {
 
 
 
+
+app.get("/:username/secrets", (req, res) => {
+  res.render("secrets", { username: req.params.username });
+})
+
+
+
+
 app.post("/login", (req, res) => {
   User.findOne({ email: req.body.username }, (err, foundUser) => {
     if (err) console.log(err);
     else {
       if (foundUser) {
         if (foundUser.password === md5(req.body.password))
-          res.render("secrets");
+          res.redirect(`/${req.body.username}/secrets`);
       }
     }
   })
@@ -79,13 +97,44 @@ app.post("/register", (req, res) => {
     else {
       const newAccount = new User({
         email: req.body.username,
-        password: md5(req.body.password)
+        password: md5(req.body.password),
+        secrets: [],
       });
 
       newAccount.save();
-      res.render("secrets");
+      res.redirect(`/${req.body.username}/secrets`);
     }
   })
+})
+
+
+
+
+app.route("/:username/submit")
+
+  .get((req, res) => {
+    res.render("submit", { username: req.params.username })
+  })
+
+  .post((req, res) => {
+    const newSecret = new Secret({
+      content: req.body.secret
+    })
+    User.findOne({ email: req.params.username }, (err, foundUser) => {
+      if (err) console.log(err);
+      else {
+        foundUser.secrets.push(newSecret);
+        foundUser.save();
+      }
+    })
+    res.redirect(`/${req.params.username}/secrets`)
+  })
+
+
+
+
+app.get("/logout", (req, res) => {
+  res.redirect("/");
 })
 
 
